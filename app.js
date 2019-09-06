6d0aaad79a5e7ce302903c3fcb92a998cf4f55d1
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-06 20:41:12 +0800

    fetching current weather data from dark sky API

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const request = require("request");
+const chalk = require("chalk");
 
-const url = "https://api.darksky.net/forecast/c8a501c907394ac466641f19b0986dc6/37.8267,-122.4233";
+const lat = "37.8267"
+const lng = "-122.4233"
+const lang = "zh-tw" || "en";
+const url = `https://api.darksky.net/forecast/c8a501c907394ac466641f19b0986dc6/${lat},${lng}?units=si&lang=${lang}`;
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+// TODO:
+// - Print a small forecast to the user
+// - Example: "It is currently 58.55 degrees outside. There is a 0% chance of rain."
+
+request({ url, json: true }, (error, response) => {
+  const data = response.body;
+
+  const tempMessage = lang === "zh-tw" ? "天氣現在" + data.currently.temperature + "度。" : "It is currently " + data.currently.temperature + "º outside. ";
+  const precipMessage = lang === "zh-tw" ? "下雨機率為" + (data.currently.precipProbability * 100) + "%。" : "There is a " + (data.currently.precipProbability * 100) + "% chance of rain.";
+
+  console.log(chalk.inverse("Selected timezone: " + data.timezone));
+  console.log(tempMessage + precipMessage);
+  console.log(data.daily.summary);
 });
