1c298796a4d9a678f6c2a444833233e463ad1934
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-08 06:57:47 +0800

    refactored as callback function

---
+++ geocode.js
@@ -0,0 +1,32 @@
+const request = require("request");
+const chalk = require("chalk");
+
+const accessToken = "pk.eyJ1Ijoid2N5am95Y2UiLCJhIjoiY2swNXg3NTlpM3B3NDNibXZqaWF0a2dlaiJ9.npfswSTgFPg3uwWzSR0KMg"
+
+const geocode = (address, callback) => {
+  let location = "";
+  address.split("").forEach(char => {
+    const letter = char === " " ? "%20" : char;
+    location += letter;
+  });
+  const geocodingURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${accessToken}&limit=1`;
+
+  request({ url: geocodingURL, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service.", undefined)
+    } else if (response.body.message === "Forbidden" || response.body.error || response.body.features.length === 0) {
+      callback("Unable to find location", undefined)
+    } else {
+      callback(undefined, {
+        place: response.body.features[0].place_name,
+        lat: response.body.features[0].center[0],
+        lng: response.body.features[0].center[1]
+      })
+    };
+  });
+};
+
+geocode("Hong Kong", (error, data) => {
+  console.log("Error: ", error);
+  console.log("Data: ", data);
+});
