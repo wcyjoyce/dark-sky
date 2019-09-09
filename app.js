cd7fea13782880d0ab283f33b8df57c3b9aabf67
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-10 07:17:24 +0800

    error handled when location is not provided

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const geocode = require("./utilities/geocode.js");
 const forecast = require("./utilities/forecast.js");
 
+// TODO:
+// 1) Input location query via command line argument without yargs
+// 2) Only geocode if a location was provided
+
 const query = process.argv[2];
 
-geocode(query, (error, data) => {
-  if (error) {
-    return console.log(error);
-  };
-
-  forecast(data.lng, data.lat, (error, forecastData) => {
+if (!query) {
+  console.log("Please provide a location.")
+} else {
+  geocode(query, (error, data) => {
     if (error) {
       return console.log(error);
     };
 
-    console.log("Location: ", data.location);
-    console.log("Forecast: ", forecastData);
+    forecast(data.lng, data.lat, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      };
+
+      console.log("Location: ", data.location);
+      console.log("Forecast: ", forecastData);
+    });
   });
-});
+};
