d6bd0ffbeff3bafeaf35a11750d1eb5fcf0cfc86
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-10 07:44:21 +0800

    cleaned code

--- utilities/forecast.js
+++ utilities/forecast.js
@@ -12,9 +12,9 @@ const forecast = (lat, lng, callback) => {
 
   request({ url: weatherURL, json: true }, (error, { body }) => {
     if (error) {
-      callback("Unable to connect to weather service", undefined)
+      callback("Unable to connect to weather service", undefined);
     } else if (body.error) {
-      callback("Unable to find location", undefined)
+      callback("Unable to find location", undefined);
     } else {
       const tempMessage = "It is currently " + body.currently.temperature + "º outside. ";
       const precipMessage = "There is a " + Math.round(body.currently.precipProbability * 100) + "% chance of rain.";
